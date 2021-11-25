9d4df5819b70d76da4ed518aa10f27974fcdb624
Filipe Gomes <filipe.gomes@mercadolivre.com>
2021-11-25 11:41:00 -0300

    Testes com 100% de cobertura em ReturnOrdersHistoryControllerTest

---
+++ src/test/java/com/mercadolibre/demo/controller/ReturnOrdersHistoryControllerTest.java
@@ -0,0 +1,179 @@
+package com.mercadolibre.demo.controller;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mercadolibre.demo.dto.TokenDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import javax.transaction.Transactional;
+import java.net.URI;
+
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Transactional
+@AutoConfigureTestEntityManager
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ReturnOrdersHistoryControllerTest {
+
+	private URI uri;
+
+	@Autowired
+	MockMvc mockMvc;
+
+	private TokenDTO tokenDTO;
+
+	@BeforeEach
+	public void testandoAutenticacao() throws Exception {
+		String json = "{\"user\": \"filipe\", \"senha\": \"123\"}";
+		uri = new URI("/auth");
+
+		MvcResult resultContendoToken = mockMvc
+				.perform(MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON).content(json))
+				.andExpect(status().isOk()).andReturn();
+		tokenDTO = new ObjectMapper().readValue(resultContendoToken.getResponse().getContentAsString(), TokenDTO.class);
+	}
+
+	@Test
+	public void testFindReturnOrdersHistoryByIdNoSucess() throws Exception {
+
+		uri = new URI("/api/v1/fresh-products/returnOrdersHistory/findById/1000");
+
+		assertNotNull(uri);
+
+		MvcResult result = mockMvc.perform(
+				MockMvcRequestBuilders.get(uri).header("Authorization", tokenDTO.getTipo() + " " + tokenDTO.getToken()))
+				.andExpect(status().isNotFound()).andReturn();
+
+		String responseJson = result.getResponse().getContentAsString();
+
+		assertNotNull(responseJson);
+	}
+	
+	@Test
+	public void testFindReturnOrdersHistoryByIdWithSucess() throws Exception {
+
+		uri = new URI("/api/v1/fresh-products/returnOrdersHistory/findById/1");
+
+		assertNotNull(uri);
+
+		MvcResult result = mockMvc.perform(
+				MockMvcRequestBuilders.get(uri).header("Authorization", tokenDTO.getTipo() + " " + tokenDTO.getToken()))
+				.andExpect(status().isOk()).andReturn();
+
+		String responseJson = result.getResponse().getContentAsString();
+
+		assertNotNull(responseJson);
+	}
+
+	@Test
+	public void testListReturnOrdersHistoryWithSucess() throws Exception {
+
+		uri = new URI("/api/v1/fresh-products/returnOrdersHistory/list");
+
+		assertNotNull(uri);
+
+		MvcResult result = mockMvc.perform(
+				MockMvcRequestBuilders.get(uri).header("Authorization", tokenDTO.getTipo() + " " + tokenDTO.getToken()))
+				.andExpect(status().isOk()).andReturn();
+
+		String responseJson = result.getResponse().getContentAsString();
+
+		assertNotNull(responseJson);
+	}
+
+	@Test
+	public void testToDiscardNoSuccess() throws Exception {
+
+		uri = new URI("/api/v1/fresh-products/returnOrdersHistory/toDiscard");
+
+		assertNotNull(uri);
+
+		String requestJson = "{\"idReturnOrders\": 1, \"discard\": \"null\", \"idPurchaseOrder\": 1}";
+		MvcResult result = mockMvc.perform(
+				MockMvcRequestBuilders.post(uri)
+				.content(requestJson)
+				.header("Content-Type", "application/json")
+				.header("Authorization", tokenDTO.getTipo() + " " + tokenDTO.getToken()))
+				.andExpect(status().isNotFound()).andReturn();
+
+		String responseJson = result.getResponse().getContentAsString();
+
+		assertNotNull(responseJson);
+	}
+
+	@Test
+	public void testToDiscardWithSuccess() throws Exception {
+
+		uri = new URI("/api/v1/fresh-products/returnOrdersHistory/toDiscard");
+
+		assertNotNull(uri);
+
+		String requestJson = "{\"idReturnOrders\": 1, \"discard\": \"Sim\", \"idPurchaseOrder\": 1}";
+
+
+		MvcResult result = mockMvc.perform(
+				MockMvcRequestBuilders.post(uri)
+				.content(requestJson)
+				.header("Content-Type", "application/json")
+				.header("Authorization", tokenDTO.getTipo() + " " + tokenDTO.getToken()))
+				.andExpect(status().isCreated()).andReturn();
+
+		String responseJson = result.getResponse().getContentAsString();
+
+		assertNotNull(responseJson);
+	}
+	
+	@Test
+	public void testIncrementNoSuccess() throws Exception {
+
+		uri = new URI("/api/v1/fresh-products/returnOrdersHistory/increment");
+
+		assertNotNull(uri);
+
+		String requestJson =  "{\"idReturnOrders\": 1000, \"discard\": \"null\", \"idPurchaseOrder\": 1}";
+
+		MvcResult result = mockMvc.perform(
+				MockMvcRequestBuilders.post(uri)
+				.content(requestJson)
+				.header("Content-Type", "application/json")
+				.header("Authorization", tokenDTO.getTipo() + " " + tokenDTO.getToken()))
+				.andExpect(status().isNotFound()).andReturn();
+
+		String responseJson = result.getResponse().getContentAsString();
+
+		assertNotNull(responseJson);
+	}
+
+	@Test
+	public void testIncrementWithSuccess() throws Exception {
+
+		uri = new URI("/api/v1/fresh-products/returnOrdersHistory/increment");
+
+		assertNotNull(uri);
+
+		String requestJson =  "{\"idReturnOrders\": 1, \"discard\": \"Não\", \"idPurchaseOrder\": 1}";
+
+
+		MvcResult result = mockMvc.perform(
+				MockMvcRequestBuilders.post(uri)
+				.content(requestJson)
+				.header("Content-Type", "application/json")
+				.header("Authorization", tokenDTO.getTipo() + " " + tokenDTO.getToken()))
+				.andExpect(status().isCreated()).andReturn();
+
+		String responseJson = result.getResponse().getContentAsString();
+
+		assertNotNull(responseJson);
+	}
+}
